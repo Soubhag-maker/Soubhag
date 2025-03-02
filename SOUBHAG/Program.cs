75e77c945938dff14db247c7c3a2dcf486f47567
soubhag <soubhag8@gmail.com>
2025-03-03 02:15:03 +0530

    Removed automatic database migration from Program.cs

--- SOUBHAG/Program.cs
+++ SOUBHAG/Program.cs
@@ -1,15 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using SOUBHAG.Data;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 
 namespace SOUBHAG
 {
@@ -17,29 +7,11 @@ namespace SOUBHAG
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-
-            // Run Database Migrations Automatically (Optional)
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<AppDbContext>();
-                    context.Database.Migrate();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while migrating the database.");
-                }
-            }
-
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-          WebHost.CreateDefaultBuilder(args)
-              .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
